fd71ad6861250f31811a4c0fc5db1fac8a997f3e
carlos1843216 <carlos1843216@gmail.com>
2021-04-23 20:41:55 -0500

    feat: Added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'bhhamvik1o9a4ybzsnf3-mysql.services.clever-cloud.com',
-      port: 3306,
-      username: 'uwmafsen4apxvdfp',
-      password: 'vcOPQnVYLVgMf1DHRc8I',
-      database: 'bhhamvik1o9a4ybzsnf3',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: false,
       keepConnectionAlive: true,
